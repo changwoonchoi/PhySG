10db8a275947fcbb17d36fe583a2f27f8cf49cef
Changwoon Choi <changwoon.choi00@gmail.com>
2023-11-15 08:18:05 +0900

    init specular_albedo=0.1

--- code/evaluate_specular_albedo.sh
+++ code/evaluate_specular_albedo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python evaluation/eval.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/armadillo" --split test --expname "tensoir_new_armadillo" --gamma 2.2 --gpu 0
-python evaluation/eval.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/ficus" --split test --expname "tensoir_new_ficus" --gamma 2.2 --gpu 0
-python evaluation/eval.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/hotdog" --split test --expname "tensoir_new_hotdog" --gamma 2.2 --gpu 0
-python evaluation/eval.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/lego" --split test --expname "tensoir_new_lego" --gamma 2.2 --gpu 0
+python evaluation/eval.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/armadillo" --split test --expname "tensoir_0.1_armadillo" --gamma 2.2 --gpu 0
+python evaluation/eval.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/ficus" --split test --expname "tensoir_0.1_ficus" --gamma 2.2 --gpu 0
+python evaluation/eval.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/hotdog" --split test --expname "tensoir_0.1_hotdog" --gamma 2.2 --gpu 0
+python evaluation/eval.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/lego" --split test --expname "tensoir_0.1_lego" --gamma 2.2 --gpu 0
--- code/train_specular_albedo.sh
+++ code/train_specular_albedo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python training/exp_runner.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/$1" --split train --expname "tensoir_new_$1" --gamma 2.2 --gpu $2
+python training/exp_runner.py --conf confs_sg/tensoir_specular_albedo.conf --data_dir "../data/tensoir/$1" --split train --expname "tensoir_0.01_$1" --gamma 2.2 --gpu $2
